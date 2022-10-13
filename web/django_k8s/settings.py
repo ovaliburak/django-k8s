cffd39e6a76aea2dfb58d8811ea038410980b0f8
Burak Ovali <burakovalii@outlook.com>
2022-10-13 18:44:36 +0300

    k8s secrets psql and settings fixed

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get("DEBUG")) == "1"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -82,41 +82,51 @@ WSGI_APPLICATION = 'django_k8s.wsgi.application'
 # }
 
 #DECLARE DATABASE VARIABLES 
-DB_USERNAME = os.environ.get("POSTGRES_USER")
-DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
-DB_DATABASE = os.environ.get("POSTGRES_DB")
-DB_HOST = os.environ.get("POSTGRES_HOST")
-DB_PORT = os.environ.get("POSTGRES_PORT")
-DB_IS_AVAILABLE = all([
-    DB_USERNAME,
-    DB_PASSWORD,
-    DB_DATABASE,
-    DB_HOST,
-    DB_PORT
-])
+# DB_USERNAME = os.environ.get("POSTGRES_USER")
+# DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+# DB_DATABASE = os.environ.get("POSTGRES_DB")
+# DB_HOST = os.environ.get("POSTGRES_HOST")
+# DB_PORT = os.environ.get("POSTGRES_PORT")
+# DB_IS_AVAILABLE = all([
+#     DB_USERNAME,
+#     DB_PASSWORD,
+#     # DB_DATABASE,
+#     DB_HOST,
+#     DB_PORT
+# ])
 # POSTGRES_READY=str(os.environ.get("POSTGRES_READY")) == '1'
 
-DB_IGNORE_SSL=os.environ.get('DB_IGNORE_SSL') == "true"
+# DB_IGNORE_SSL=os.environ.get('DB_IGNORE_SSL') == "true"
 
-if DB_IS_AVAILABLE:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': DB_DATABASE,
-        'USER': DB_USERNAME,
-        'PASSWORD': DB_PASSWORD,
-        'HOST': DB_HOST,
-        'PORT': DB_PORT,
-    }
-}
+# if DB_IS_AVAILABLE:
+#     DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'kubernetes_django',
+#         'USER': DB_USERNAME,
+#         'PASSWORD': DB_PASSWORD,
+#         'HOST': DB_HOST,
+#         'PORT': DB_PORT,
+#     }
+# }
 
-    if not DB_IGNORE_SSL:
-        DATABASES["default"]["OPTIONS"]= {
-            "sslmode":"required"
-        }
+    # if not DB_IGNORE_SSL:
+    #     DATABASES["default"]["OPTIONS"]= {
+    #         "sslmode":"required"
+    #     }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kubernetes_django',
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT', 5432)     }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
